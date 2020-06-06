90c47b88182f0d96a7641a47f938e5a52d118a16
DreTaX <dretax14@gmail.com>
2020-06-06 18:53:23 +0200

    Duh.
    Signed-off-by: DreTaX <dretax14@gmail.com>

--- Garhal/memory.h
+++ Garhal/memory.h
@@ -2,6 +2,10 @@
 #include <ntifs.h>
 #include "vector.h"
 
+ULONG GetWindowsBuildNumber();
+
+int GetCorrectOffset(CHAR* Name, ULONG BuildNumber);
+
 NTSTATUS KeReadVirtualMemory(PEPROCESS Process, PVOID SourceAddress, PVOID TargetAddress, SIZE_T Size);
 
 NTSTATUS KeWriteVirtualMemory(PEPROCESS Process, PVOID SourceAddress, PVOID TargetAddress, SIZE_T Size);
