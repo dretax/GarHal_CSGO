e70880603ae4b37bdac8f36cac2de2b7d1ea5cba
DreTaX <dretax14@gmail.com>
2022-11-03 23:58:42 +0100

    Revert "fixed world2screen function"
    This reverts commit da0989b5b07fae5c86b7d6017d8c83394f4eaea1.

--- LEGACY_GarhalController/Engine.cpp
+++ LEGACY_GarhalController/Engine.cpp
@@ -1,6 +1,5 @@
 #include "Engine.hpp"
 #include "offsets.hpp"
-#include "imgui.h"
 
 // hazedumper namespace
 using namespace hazedumper::netvars;
@@ -10,14 +9,27 @@ bool Engine::worldToScreen(const Vector3& from, Vector3& to)
 {
     WorldToScreenMatrix matrix = Driver.ReadVirtualMemory<WorldToScreenMatrix>(ProcessId, ClientAddress + dwViewMatrix, sizeof(WorldToScreenMatrix));
 
-    const auto w = matrix(3, 0) * from(0) + matrix(3, 1) * from(1) + matrix(3, 2) * from(2) + matrix(3, 3);
-    if (w < 0.001f)
+    float w = 0.0f;
+
+    to(0) = matrix(0, 0) * from(0) + matrix(0, 1) * from(1) + matrix(0, 2) * from(2) + matrix(0, 3);
+    to(1) = matrix(1, 0) * from(0) + matrix(1, 1) * from(1) + matrix(1, 2) * from(2) + matrix(1, 3);
+    w = matrix(3, 0) * from(0) + matrix(3, 1) * from(1) + matrix(3, 2) * from(2) + matrix(3, 3);
+
+    if (w < 0.01f)
         return false;
 
-    to(0) = ImGui::GetIO().DisplaySize.x / 2.0f;
-    to(1) = ImGui::GetIO().DisplaySize.y / 2.0f;
-    to(0) *= 1.0f + (matrix(0, 0) * from(0) + matrix(0, 1) * from(1) + matrix(0, 2) * from(2) + matrix(0, 3)) / w;
-    to(1) *= 1.0f - (matrix(1, 0) * from(0) + matrix(1, 1) * from(1) + matrix(1, 2) * from(2) + matrix(1, 3)) / w;
+    float wInverse = 1.0f / w;
+    to(0) *= wInverse;
+    to(1) *= wInverse;
+
+    float x = SCREEN_WIDTH / 2;
+    float y = SCREEN_HEIGHT / 2;
+
+    x += 0.5 * to(0) * SCREEN_WIDTH + 0.5;
+    y -= 0.5 * to(1) * SCREEN_HEIGHT + 0.5;
+
+    to(0) = x;
+    to(1) = y;
 
     return true;
 }
